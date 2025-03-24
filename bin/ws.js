0626aa6ad041bfa421fae47cffeca367d4246b4a
Protorians <protorians.dev@gmail.com>
2025-03-24 16:08:41 +0000

    Fix workflow

--- bin/ws.js
+++ bin/ws.js
@@ -9,18 +9,18 @@ const directory = process.cwd();
 ws.name('mode-manager')
     .command('mode')
     .description('Project mode manager')
-    .argument("<string>", "Set mode (dev, prod)",)
+    // .argument("<string>", "Set mode (dev, prod)",)
     .action((mode) => {
-        if (mode === 'dev') {
+        // if (mode === 'dev') {
             console.log('TASK', 'Active DEV workspace mode...')
             execSync("chmod +x .githooks/pre-push")
             setGitHooksPath()
-        }
-        else if (mode === 'prod') {
-            console.log('TASK', 'Active PROD workspace mode...')
-            unsetGitHooksPath()
-        } else
-            console.log('TASK', 'Mode not available <', mode, '>')
+        // }
+        // else if (mode === 'prod') {
+        //     console.log('TASK', 'Active PROD workspace mode...')
+        //     unsetGitHooksPath()
+        // } else
+        //     console.log('TASK', 'Mode not available <', mode, '>')
     })
 
 
