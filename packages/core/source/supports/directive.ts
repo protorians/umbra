cd7fe6786e06d280c427145b0728f63b518f07bf
Protorians <129060085+protorians@users.noreply.github.com>
2025-05-13 14:54:56 +0000

    Refactor directive handling and introduce utility types
    Refactored directive handling by updating class and type definitions for better clarity and extensibility. Added `IDirectiveStack` and `TreatmentQueueStatus` to support enhanced directive processing logic. Introduced methods like `remove` and `clear` to improve flexibility in managing directives.

--- packages/core/source/supports/directive.ts
+++ packages/core/source/supports/directive.ts
@@ -1,9 +1,10 @@
-import type {IDirective, IDirectives, IDirectivesCollection} from "../types";
+import type {IDirective, IDirectives, IDirectivesCollection, IDirectiveStack} from "../types";
+import {TreatmentQueueStatus} from "./enums";
 
-export class Directive<I> {
-    protected _stack: IDirectives<I> = new Map<string, IDirective<I>>();
+export class Directives<I> implements IDirectives<I> {
+    protected _stack: IDirectiveStack<I> = new Map<string, IDirective<I>>();
 
-    stack(): IDirectives<I> {
+    stack(): IDirectiveStack<I> {
         return this._stack;
     }
 
@@ -17,16 +18,34 @@ export class Directive<I> {
         return this._stack.get(id);
     }
 
-    add(id: string, value: IDirective<I>): typeof this {
+    add(id: string, value: IDirective<I>): this {
         this._stack.set(id, value);
         return this;
     }
 
-    process(entry: I): I {
-        for (const directive of Object.values(this.directives))
-            entry = directive(entry);
-        return entry;
+    remove(id: string): this {
+        this._stack.delete(id);
+        return this;
     }
 
+    clear(): this {
+        this._stack.clear();
+        return this;
+    }
+
+    process<K>(value: I, type?: string, args?: Record<string, any>): K {
+        for (const [id, directive] of Object.entries(this.directives)) {
+            const parsed = directive({value, type, args});
+
+            if (parsed === TreatmentQueueStatus.Exit || parsed === TreatmentQueueStatus.Cancel)
+                return value as any;
+
+            else if (parsed === TreatmentQueueStatus.SnapOut)
+                this.remove(id)
+
+            else if (typeof parsed !== 'undefined') value = parsed as any;
+        }
+        return value as any;
+    }
 }
 
--- packages/core/source/types/directive.ts
+++ packages/core/source/types/directive.ts
@@ -1,5 +1,34 @@
-export type IDirectives<I> = Map<string, IDirective<I>>;
+import {TreatmentQueueStatus} from "../supports";
+
+export type IDirectiveStack<I> = Map<string, IDirective<I>>;
 
 export type IDirectivesCollection<I> = Record<string, IDirective<I>>;
 
-export type IDirective<I> = (entry: I) => I;
+export interface IDirectivePayload<I> {
+    value: I;
+    type?: string;
+    args?: Record<string, any>;
+}
+
+export type IDirective<I> = (payload: IDirectivePayload<I>) => I
+    | TreatmentQueueStatus.Exit
+    | TreatmentQueueStatus.Cancel
+    | TreatmentQueueStatus.SnapOut
+    | undefined
+    | void;
+
+export interface IDirectives<I> {
+    stack(): IDirectiveStack<I>;
+
+    get directives(): IDirectivesCollection<I>;
+
+    directive(id: string): IDirective<I> | undefined;
+
+    add(id: string, value: IDirective<I>): this;
+
+    remove(id: string): this;
+
+    clear(): this;
+
+    process<K>(entry: I, type?: string, args?: Record<string, any>): K;
+}
